1074b4655d7c9c4368db5bcd46e379d2605f68e0
aliiimaher <alimohammadmaher@gmail.com>
2026-03-06 09:49:45 -0500

    feat: create the mapper function for converting dto to our domain.

---
+++ app/models/mapper/TmdbMapper.java
@@ -0,0 +1,58 @@
+package models.mapper;
+
+import models.domain.Movie;
+import models.domain.TVShow;
+import models.dto.MovieDTO;
+import models.dto.TVShowDTO;
+
+/**
+ * This is domain model for representing Movie.
+ * The fields are based on the Appendix of the Project Document.
+ *
+ * @author  Ali Maher
+ */
+
+public class TmdbMapper {
+    public static Movie toMovie(MovieDTO movieDTO) {
+        return new Movie(
+                movieDTO.id,
+                movieDTO.title,
+                movieDTO.overview,
+                movieDTO.release_date,
+                movieDTO.genres,
+                movieDTO.homepage,
+                movieDTO.popularity,
+                movieDTO.poster_path,
+                movieDTO.production_companies,
+                movieDTO.revenue,
+                movieDTO.runtime,
+                movieDTO.spoken_languages,
+                movieDTO.status,
+                movieDTO.tagline,
+                movieDTO.vote_average,
+                movieDTO.vote_count
+        )
+    }
+
+    public static TVShow toTVShow(TVShowDTO tvShowDTO) {
+        return new TVShow(
+                tvShowDTO.id,
+                tvShowDTO.name,
+                tvShowDTO.overview,
+                tvShowDTO.first_air_date,
+                tvShowDTO.last_air_date,
+                tvShowDTO.popularity,
+                tvShowDTO.poster_path,
+                tvShowDTO.genres,
+                tvShowDTO.homepage,
+                tvShowDTO.number_of_episodes,
+                tvShowDTO.number_of_seasons,
+                tvShowDTO.networks,
+                tvShowDTO.status,
+                tvShowDTO.tagline,
+                tvShowDTO.vote_average,
+                tvShowDTO.vote_count,
+                tvShowDTO.type
+        );
+    }
+}
