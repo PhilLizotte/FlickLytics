82265620eb8da6389ae05c95c48efcf715c79dd0
Craig <craigk00@hotmail.com>
2026-03-08 13:47:43 -0400

    fetching 50 reviews

--- app/services/tmdb/TmdbSearchService.java
+++ app/services/tmdb/TmdbSearchService.java
@@ -86,26 +86,51 @@ public class TmdbSearchService {
     }
 
     private CompletionStage<ArrayList<String>> fetchReviewsList(String kind, int id) {
-    String url = tmdbConfig.getBaseUrl() + "/" + kind + "/" + id + "/reviews";
-    // BUG:: for some reason, this is only fetching the first 20 reviews, not all of them. 
-    WSRequest request = ws.url(url)
-        .addQueryParameter("api_key", tmdbConfig.getApiKey())
-        .addQueryParameter("language", "en-us");
+        String url = tmdbConfig.getBaseUrl() + "/" + kind + "/" + id + "/reviews";
+        WSRequest request1 = ws.url(url)
+            .addQueryParameter("api_key", tmdbConfig.getApiKey())
+            .addQueryParameter("language", "en-us")
+            .addQueryParameter("page", "1");
+        
+        WSRequest request2 = ws.url(url)
+            .addQueryParameter("api_key", tmdbConfig.getApiKey())
+            .addQueryParameter("language", "en-us")
+            .addQueryParameter("page", "2");
+        
+        WSRequest request3 = ws.url(url)
+            .addQueryParameter("api_key", tmdbConfig.getApiKey())
+            .addQueryParameter("language", "en-us")
+            .addQueryParameter("page", "3");
 
-        return request.get().thenApply(resp -> {
-            JsonNode json = resp.asJson();
-            ArrayList<String> list = new ArrayList<String>();
-            
-            JsonNode reviewsNode = json.get("results");
-            if (reviewsNode != null && reviewsNode.isArray())
-            {
-                for (JsonNode r : reviewsNode)
-                {
-                    list.add(r.get("content").asText());
-                }
-            }
-            return list;
-        });
+
+        return request1.get().thenCompose(resp1 -> 
+            request2.get().thenCompose(resp2 -> 
+                request3.get().thenApply(resp3 -> {
+                    ArrayList<String> list = new ArrayList<String>();
+                    JsonNode res1 = resp1.asJson().get("results");
+                    JsonNode res2 = resp2.asJson().get("results");
+                    JsonNode res3 = resp3.asJson().get("results");
+
+                    if (res1 != null && res1.isArray())
+                        for (JsonNode r : res1)
+                            list.add(r.get("content").asText());
+
+                    if (res2 != null && res2.isArray())
+                        for (JsonNode r : res2)
+                            list.add(r.get("content").asText());
+
+                    if (res3 != null && res3.isArray())
+                        for (JsonNode r : res3)
+                        {
+                            list.add(r.get("content").asText());
+                            if (list.size() == 50)
+                                break;
+                        }
+                    System.out.print(list.size() + ", ");
+                    return list;
+                })
+            )
+        );
     }
 
     private JsonNode enrichResults(String category, JsonNode searchJson, Map<Integer, String> genreMap) {
